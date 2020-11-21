96a93fedce499012fe4d903c5b4d78f2aee61f2d
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2020-11-21 16:00:25 -0600

    slight changes

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ $.ajax({
 }).then(function(response) {
     let currTime = new Date(response.dt*1000);
     currTime.getMonth()+1/currTime.getDate()/currTime.getFullYear();
-    console.log(currTime)
+    // console.log(currTime)
 
 
     var icon = (response.weather[0].icon);
-    console.log(icon);
+    // console.log(icon);
     temp.text("Temperature: "+response.main.temp+" F");
     hum.text("Humidity: "+response.main.humidity+"%");
     windspeed.text("Windspeed: "+response.wind.speed+"m/s");
@@ -43,20 +43,20 @@ $.ajax({
     }).then(function(response){
         uvIndex.text("Uvi: "+response.value);
        
-        $("#uvIndex").attr("background-color","yellow");
+        uvIndex.attr(style="background-color","green");
 
         if (uvIndex > 11) {
             uvIndex.style.backgroundcolor = "purple";
-        } else if (uvIndex > 8) {
-            uviSeverity = "red";
-        } else if (uvIndex > 6) {
+        } else if (uvIndex >= 8) {
+            uvIndex.style.backgroundcolor = "yellow";
+        } else if (uvIndex >= 6) {
             uvIndex.style.backgroundcolor = "orange";
             
-        } else if (uvIndex > 3) {
+        } else if (uvIndex >= 3) {
             uvIndex.style.backgroundcolor = "yellow";
          
         }
-       
+       console.log(response.value)
     
     });
 var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=43f88cf4c22e52ae2848c5fb7e859150&units=imperial"
@@ -65,7 +65,7 @@ var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=
         fiveDay,
         method: "GET"
     }).then(function(response){
-        console.log(fiveDay);
+        // console.log(fiveDay);
 
     });
 });
