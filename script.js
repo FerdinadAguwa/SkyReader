d3b927963ef79c82ee42094fe30818171c5a7c46
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2020-11-21 20:11:38 -0600

    changes to java/html

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (var i = 0; i < localStorage.length; i++) {
 
     var city = localStorage.getItem(i);
     
-    var cityName = $(".foreCastList").addClass("listGroupItem");
+    var cityName = $(".foreCastList").addClass("forcastList");
 
     cityName.append("<li>" + city + "</li>");
 }
@@ -25,18 +25,3 @@ searchButton.click(function () {
     // Variable for 5 day forecast working
     var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
 
-
-    if (searchInput == "") {
-        console.log(searchInput);
-    } else {
-        $.ajax({
-            url: urlCurrent,
-            method: "GET"
-        }).then(function (response) {
-            // foreCastlist append an li to it with just set text
-            // console.log(response.name);
-            var cityName = $(".foreCastList").addClass("listGroupItem");
-            cityName.append("<li>" + response.name + "</li>");
-            // Local storage
-            var local = localStorage.setItem(keyCount, response.name);
-            keyCount = keyCount + 1;
